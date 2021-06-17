908ce2f5331542efd44b66d8e723ac3aba8553f9
Yury Kundin <kundin.y@divan.ru>
2021-06-17 14:30:24 +0300

    Redesign the custom scroller

--- client/UI/Scroller/Scroller.tsx
+++ client/UI/Scroller/Scroller.tsx
@@ -7,27 +7,66 @@ import styles from './Scroller.module.css';
 export interface ScrollerProps {
   className?: string;
   space?: number;
+  invisible?: boolean;
   children: ReactChild | ReactChild[];
 }
 
 const Scroller: FC<ScrollerProps> = (props) => {
-  const { className, children, space = 0, ...restProps } = props;
+  const { className, children, space = 0, invisible, ...restProps } = props;
 
   return (
     <Scrollbar
       {...restProps}
-      className={cn(styles.scroller, className)}
+      className={cn(styles.scroller, { [styles.invisible]: invisible }, className)}
       noScrollX
+      noDefaultStyles
+      wrapperProps={{
+        renderer: (scrollbarProps) => {
+          const { elementRef, ...restScrollbarProps } = scrollbarProps;
+
+          return <div {...restScrollbarProps} ref={elementRef} className={styles.wrapper} />;
+        },
+      }}
+      scrollerProps={{
+        renderer: (scrollbarProps) => {
+          const { elementRef, ...restScrollbarProps } = scrollbarProps;
+
+          return (
+            <div {...restScrollbarProps} ref={elementRef} className={styles.scrollerContainer} />
+          );
+        },
+      }}
+      contentProps={{
+        renderer: (scrollbarProps) => {
+          const { elementRef, ...restScrollbarProps } = scrollbarProps;
+
+          return (
+            <div
+              {...restScrollbarProps}
+              ref={elementRef}
+              className={styles.content}
+              style={{ paddingRight: `${space}px` }}
+            />
+          );
+        },
+      }}
       trackYProps={{
+        renderer: (scrollbarProps) => {
+          const { elementRef, ...restScrollbarProps } = scrollbarProps;
+
+          return <div {...restScrollbarProps} className={styles.trackY} ref={elementRef} />;
+        },
+      }}
+      thumbYProps={{
         renderer: (scrollbarProps) => {
           const { elementRef, style, ...restScrollbarProps } = scrollbarProps;
 
           return (
             <div
               {...restScrollbarProps}
-              className={styles.trackY}
               ref={elementRef}
-              style={{ ...style, paddingRight: `${space}px` }}
+              className={styles.thumbY}
+              style={{ height: style.height, transform: style.transform }}
             />
           );
         },
