6e516555f012aff11fd7eb6d4ce7bb760bf46ef5
Yury Kundin <kundin.y@divan.ru>
2021-06-18 17:36:22 +0300

    Remove unused useMeta

--- client/App/App.tsx
+++ client/App/App.tsx
@@ -3,7 +3,6 @@ import loadable from '@loadable/component';
 import { Switch, Route } from 'react-router-dom';
 
 import ModalsProvider from '@Contexts/Modals/ModalsProvider';
-import useMeta from '@Hooks/useMeta';
 import '@UI/fonts.module.css';
 import '@UI/vars.module.css';
 import './App.css';
@@ -18,8 +17,6 @@ const PageCategory = loadable(() => import('@Pages/PageCategory'));
 const TemplateMain = loadable(() => import('@Templates/TemplateMain'));
 
 const App: FC = () => {
-  useMeta();
-
   return (
     <ModalsProvider>
       <Switch>
