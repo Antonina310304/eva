676e93625d51dde9d379eb1da4e242187e228cef
Yury Kundin <kundin.y@divan.ru>
2021-07-12 16:59:08 +0300

    Add section with recently watched products

--- client/Pages/PageProduct/elements/CrossSaleSection/CrossSaleSection.tsx
+++ client/Pages/PageProduct/elements/CrossSaleSection/CrossSaleSection.tsx
@@ -1,24 +1,35 @@
-import React, { FC, HTMLAttributes, memo, useCallback, useState } from 'react';
+import React, {
+  FC,
+  HTMLAttributes,
+  ReactElement,
+  memo,
+  useCallback,
+  useState,
+  cloneElement,
+} from 'react';
 import cn from 'classnames';
 
 import Section from '@Components/Section';
 import Gallery, { ProgressOptions } from '@UI/Gallery';
-import CrossSaleProductCard from '@Components/CrossSaleProductCard';
 import ButtonTabs, { Tab } from '@UI/ButtonTabs';
 import ProgressBar from '@UI/ProgressBar';
-import List from '@UI/List';
 import { ProductData } from '@Types/Product';
 import styles from './CrossSaleSection.module.css';
+
+export interface RenderItem {
+  product: ProductData;
+}
 
 export interface CrossSaleSectionProps extends HTMLAttributes<HTMLDivElement> {
   className?: string;
   title: string;
   products: ProductData[];
+  renderItem?: (props: RenderItem) => ReactElement;
   tabs?: Tab[];
 }
 
 const CrossSaleSection: FC<CrossSaleSectionProps> = (props) => {
-  const { className, title, products, tabs = [], ...restProps } = props;
+  const { className, title, products, tabs = [], renderItem, ...restProps } = props;
   const [slide, setSlide] = useState(0);
   const [track, setTrack] = useState<ProgressOptions>(null);
 
@@ -41,11 +52,11 @@ const CrossSaleSection: FC<CrossSaleSectionProps> = (props) => {
           onChangeCurrent={handleChangeCurrent}
           onChangeProgress={handleChangeProgress}
         >
-          {products.map((product) => (
-            <div className={styles.item} key={product.id}>
-              <CrossSaleProductCard product={product} />
-            </div>
-          ))}
+          {products.map((product) => {
+            const item = renderItem({ product });
+
+            return cloneElement(item, { ...item.props, key: product.id });
+          })}
         </Gallery>
 
         <ProgressBar track={track} />
