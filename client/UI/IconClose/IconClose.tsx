6c3cfb8b8ae1e0db14f17e5d0aeaa10856368752
Yury Kundin <kundin.y@divan.ru>
2021-07-30 16:26:05 +0300

    feature: Add view for icon

--- client/UI/IconClose/IconClose.tsx
+++ client/UI/IconClose/IconClose.tsx
@@ -5,12 +5,22 @@ import styles from './IconClose.module.css';
 
 export interface IconCloseProps extends HTMLAttributes<HTMLDivElement> {
   className?: string;
+  view?: 'default' | 'circle';
 }
 
 const IconClose: FC<IconCloseProps> = (props) => {
-  const { className, ...restProps } = props;
+  const { className, view = 'default', ...restProps } = props;
 
-  return <div {...restProps} className={cn(styles.iconClose, className)} />;
+  return (
+    <div
+      {...restProps}
+      className={cn(
+        styles.iconClose,
+        { [styles.viewDefault]: view === 'default', [styles.viewCircle]: view === 'circle' },
+        className,
+      )}
+    />
+  );
 };
 
 export default memo(IconClose);
