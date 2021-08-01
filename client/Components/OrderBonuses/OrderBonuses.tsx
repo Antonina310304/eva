855355f1c083687516e2ad204ad263f228f71a49
Yury Kundin <kundin.y@divan.ru>
2021-08-02 01:46:47 +0300

    feature: Add popup with info about bonuses for order

--- client/Components/OrderBonuses/OrderBonuses.tsx
+++ client/Components/OrderBonuses/OrderBonuses.tsx
@@ -1,10 +1,12 @@
-import React, { FC, HTMLAttributes, memo } from 'react';
+import React, { FC, HTMLAttributes, memo, useCallback, useState } from 'react';
 import cn from 'classnames';
 
 import Link from '@UI/Link';
+import Popup from '@UI/Popup';
 import CounterPoints from '@Components/CounterPoints';
 import useProfile from '@Queries/useProfile';
 import useOrderBonuses from '@Queries/useOrderBonuses';
+import useOnClickOutside from '@Hooks/useOnClickOutside';
 import styles from './OrderBonuses.module.css';
 
 export type OrderBonusesStatus = 'loading' | 'resolved' | 'rejected';
@@ -16,9 +18,20 @@ export interface OrderBonusesProps extends HTMLAttributes<HTMLDivElement> {
 
 const OrderBonuses: FC<OrderBonusesProps> = (props) => {
   const { className, productIds, ...restProps } = props;
+  const [visiblePopup, setVisiblePopup] = useState(false);
   const profile = useProfile();
   const orderBonuses = useOrderBonuses({ productIds });
   const extra = orderBonuses.isSuccess && orderBonuses.data.extraBonus;
+
+  const handleClickBonuses = useCallback(() => {
+    setVisiblePopup((prev) => !prev);
+  }, []);
+
+  const handleClickOutside = useCallback(() => {
+    setVisiblePopup(false);
+  }, []);
+
+  const refPopup = useOnClickOutside(handleClickOutside);
 
   return (
     <div {...restProps} className={cn(styles.bonuses, { [styles.extra]: extra }, className)}>
@@ -37,27 +50,47 @@ const OrderBonuses: FC<OrderBonusesProps> = (props) => {
       {orderBonuses.isSuccess && (
         <>
           <div className={styles.iconPoints} />
-          <CounterPoints className={styles.count} income count={orderBonuses.data.earnedAmount} />
-          <div className={styles.text}>за заказ</div>
 
-          <div className={styles.popup}>
-            {profile.data ? (
-              <>
-                {`Бонусы начисляются после оформления заказа и станут доступны через 14 дней после доставки товара. `}
-                <Link to='/site/divan-club' target='_blank'>
-                  Подробнее о программе лояльности
-                </Link>
-              </>
-            ) : (
-              <>
-                {`Зарегистрируйтесь или авторизуйтесь в программе Divan Club. Бонусы начисляются сразу
+          <div className={styles.wrapperCount} ref={refPopup}>
+            <CounterPoints
+              className={styles.count}
+              income
+              count={orderBonuses.data.earnedAmount}
+              onClick={handleClickBonuses}
+            />
+            <Popup className={styles.popup} visible={visiblePopup}>
+              {profile.data ? (
+                <>
+                  {`Бонусы начисляются после оформления заказа и станут доступны через 14 дней после доставки товара. `}
+                  <Link
+                    className={styles.link}
+                    to='/site/divan-club'
+                    target='_blank'
+                    view='native'
+                    size='s'
+                  >
+                    Подробнее о программе лояльности
+                  </Link>
+                </>
+              ) : (
+                <>
+                  {`Зарегистрируйтесь или авторизуйтесь в программе Divan Club. Бонусы начисляются сразу
                 после оформления заказа и доступны для списания через 14 дней после доставки товара. `}
-                <Link to='/site/divan-club' target='_blank'>
-                  Подробнее о программе лояльности
-                </Link>
-              </>
-            )}
+                  <Link
+                    className={styles.link}
+                    to='/site/divan-club'
+                    target='_blank'
+                    view='native'
+                    size='s'
+                  >
+                    Подробнее о программе лояльности
+                  </Link>
+                </>
+              )}
+            </Popup>
           </div>
+
+          <div className={styles.text}>за заказ</div>
         </>
       )}
     </div>
