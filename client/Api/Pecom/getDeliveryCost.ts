dbd5e146e1746e19f2f2c8bd06ff1676076a9aef
Yury Kundin <kundin.y@divan.ru>
2021-08-01 11:58:13 +0300

    feature: Add API for work with DaData and Pecom

---
+++ client/Api/Pecom/getDeliveryCost.ts
@@ -0,0 +1,20 @@
+import { Api } from '@Api/index';
+
+/**
+ * Получить стоимость доставки
+ */
+const getDeliveryCost = async (body: unknown): Promise<any> => {
+  const services = Api.getServices();
+  const url = `${services.shipping}/get-cost`;
+
+  const response = await Api.queryProxi<any>(url, {
+    method: 'POST',
+    body: JSON.stringify(body),
+  });
+
+  if (!response.ok) return Promise.reject(response);
+
+  return response.data;
+};
+
+export default getDeliveryCost;
---
+++ client/Api/Pecom/index.ts
@@ -0,0 +1,12 @@
+import getAddressHints from './getAddressHints';
+import getDeliveryCost from './getDeliveryCost';
+import getDeliveryCostByRegion from './getDeliveryCostByRegion';
+import getWarehouses from './getWarehouses';
+
+export const ApiPecom = {
+  getAddressHints,
+  getDeliveryCost,
+  getDeliveryCostByRegion,
+  getWarehouses,
+};
+export default null;
