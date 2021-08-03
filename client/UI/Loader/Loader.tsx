55360f9afa222f567796d115bfa3b708a5d94592
Yury Kundin <kundin.y@divan.ru>
2021-08-03 11:51:26 +0300

    feature: Add loader

---
+++ client/UI/Loader/Loader.tsx
@@ -0,0 +1,14 @@
+import React, { memo, HTMLAttributes, FC } from 'react';
+import cn from 'classnames';
+
+import styles from './Loader.module.css';
+
+export interface LoaderProps extends HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
+
+const Loader: FC<LoaderProps> = ({ className, ...restProps }) => {
+  return <div {...restProps} className={cn(styles.loader, className)} />;
+};
+
+export default memo(Loader);
