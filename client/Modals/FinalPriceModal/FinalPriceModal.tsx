ac77d06ccf54b9a2964fb0c53110223b60f37be1
Olga Ismailova <ismailova.o@divan.ru>
2021-08-06 18:08:59 +0500

    refactor: Remove unused imports

--- client/Modals/FinalPriceModal/FinalPriceModal.tsx
+++ client/Modals/FinalPriceModal/FinalPriceModal.tsx
@@ -1,14 +1,12 @@
 import React, { FC, memo } from 'react';
 import cn from 'classnames';
 
-import useModals from '@Hooks/useModals';
 import ModalSidebar, { ModalSidebarProps } from '@Components/ModalSidebar';
 
 import styles from './FinalPriceModal.module.css';
 
 const FinalPriceModal: FC<ModalSidebarProps> = (props) => {
   const { className, modal, ...restProps } = props;
-  const [, { closeModal }] = useModals();
 
   return (
     <ModalSidebar
