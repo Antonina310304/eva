83e1441cc92371333957b330877ed862925f0886
Yury Kundin <kundin.y@divan.ru>
2021-08-06 09:51:09 +0300

    refactor:  Improve types

--- client/Stores/relatedProducts/relatedProducts.ts
+++ client/Stores/relatedProducts/relatedProducts.ts
@@ -1,26 +1,35 @@
 import { createStore, getValue, createDerived } from '@kundinos/nanostores';
 import { useStore } from '@kundinos/nanostores/react';
 
+import { RelatedProductsListData } from '@Types/RelatedProducts';
+
 export interface ChangeQuantityProductParams {
   listId: number;
   productId: number;
   quantity: number;
 }
 
-export interface RelatedProductsStore {
+export interface RelatedProductsValue {
   productId: number;
-  lists: any[];
+  lists: RelatedProductsListData[];
 }
 
-export type UseRelatedProducts = (initialValue?: RelatedProductsStore) => RelatedProductsStore;
+export type UseRelatedProducts = (
+  initialValue?: RelatedProductsValue,
+) => {
+  changeQuantityProduct: ChangeQuantityProduct;
+  allLists: RelatedProductsListData[];
+  selectedLists: RelatedProductsListData[];
+};
+
 export type ChangeQuantityProduct = (params: ChangeQuantityProductParams) => void;
 
-const store = createStore<RelatedProductsStore>();
+const store = createStore<RelatedProductsValue>();
 
 const selectedLists = createDerived(store, ({ lists }) => {
   return lists.map((item) => ({
     ...item,
-    products: item.products.filter((product: any) => product.quantity > 0),
+    products: item.products.filter((product) => product.quantity > 0),
   }));
 });
 
@@ -32,7 +41,7 @@ const changeQuantityProduct: ChangeQuantityProduct = ({ listId, productId, quant
     lists: prevValue.lists.map((list) => {
       if (list.id !== listId) return list;
 
-      const newProducts = list.products.map((product: any) => {
+      const newProducts = list.products.map((product) => {
         if (product.id !== productId) return product;
 
         return { ...product, quantity };
@@ -46,7 +55,7 @@ const changeQuantityProduct: ChangeQuantityProduct = ({ listId, productId, quant
   });
 };
 
-export const useRelatedProducts = (initialValue?: RelatedProductsStore): any => {
+export const useRelatedProducts: UseRelatedProducts = (initialValue) => {
   const value = getValue(store);
 
   if (initialValue && value?.productId !== initialValue?.productId) {
