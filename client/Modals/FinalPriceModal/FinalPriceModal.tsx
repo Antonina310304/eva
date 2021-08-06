0047320cd6233c44b1c67ac76faa963669fe3288
Olga Ismailova <ismailova.o@divan.ru>
2021-08-06 17:58:53 +0500

    feature: Add modal for desktop

--- client/Modals/FinalPriceModal/FinalPriceModal.tsx
+++ client/Modals/FinalPriceModal/FinalPriceModal.tsx
@@ -1,4 +1,4 @@
-import React, { FC, memo, useCallback, useState, useEffect, useMemo } from 'react';
+import React, { FC, memo } from 'react';
 import cn from 'classnames';
 
 import useModals from '@Hooks/useModals';
@@ -10,7 +10,22 @@ const FinalPriceModal: FC<ModalSidebarProps> = (props) => {
   const { className, modal, ...restProps } = props;
   const [, { closeModal }] = useModals();
 
-  return <ModalSidebar modal={modal} view='default' />;
+  return (
+    <ModalSidebar
+      {...restProps}
+      className={cn(styles.wrapper, className)}
+      title='Финальная цена'
+      modal={modal}
+      view='default'
+    >
+      <div className={styles.description}>
+        Мы установили минимальную цену на этот товар, чтобы вы могли приобрести его выгодно.
+      </div>
+      <div className={styles.info}>
+        Поэтому на эту продукцию не действуют другие специальные предложения и скидки по промокодам.
+      </div>
+    </ModalSidebar>
+  );
 };
 
 export default memo(FinalPriceModal);
