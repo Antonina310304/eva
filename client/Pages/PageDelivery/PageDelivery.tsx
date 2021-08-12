4f7eda9edfe23f40291961fb0efff4a697b62fe5
Torseeva Julia <torseeva.y@divan.ru>
2021-08-12 17:22:32 +0300

    feature: Add PageDelivery

--- client/Pages/PageDelivery/PageDelivery.tsx
+++ client/Pages/PageDelivery/PageDelivery.tsx
@@ -8,9 +8,17 @@ import ShippingCostCalculator from '@Components/ShippingCostCalculator';
 import { MetaData } from '@Types/Meta';
 import ButtonTabs from '@UI/ButtonTabs';
 import List from '@UI/List';
+import Link from '@UI/Link';
+import Image from '@UI/Image';
+import FormattedText from '@Pages/PageDelivery/FormattedText';
+import OrderedList from './elements/OrderedList';
 import FreeDeliveryBanner from './elements/FreeDeliveryBanner';
+import Attention from './elements/Attention';
+import SuburbTable from './elements/SuburbTable';
 import './PageDelivery-Raw.css';
 import styles from './PageDelivery.module.css';
+
+import carImage from './car.svg';
 
 export interface PageDeliveryProps extends HTMLAttributes<HTMLDivElement> {
   className?: string;
@@ -20,13 +28,25 @@ export interface PageDeliveryProps extends HTMLAttributes<HTMLDivElement> {
 
 const PageDelivery: FC<PageDeliveryProps> = (props) => {
   const { className, page, meta, ...restProps } = props;
-  const { breadcrumbs, title, pageMenu, teaser, table, additionalCost } = page;
+  const {
+    breadcrumbs,
+    title,
+    pageMenu,
+    teaser,
+    table,
+    additionalCost,
+    acceptanceRules,
+    acceptanceProduct,
+    attention,
+  } = page;
   const [currentTab, setCurrentTab] = useState('0');
 
   const tabs = [
     { id: '0', label: 'Доставка' },
     { id: '1', label: 'Сборка' },
   ];
+
+  const isRus = meta.country === 'RUS';
 
   const handleChangeTab = useCallback((e, tab) => {
     setCurrentTab(tab.id);
@@ -45,12 +65,12 @@ const PageDelivery: FC<PageDeliveryProps> = (props) => {
         <ShippingCostCalculator className={styles.calculator} />
         <div className={styles.teaser}>{teaser}</div>
         <FreeDeliveryBanner className={styles.freeDelivery} />
-        {meta.country === 'BLR' && <FreeDeliveryBanner className={styles.freeDelivery} />}
+        {!isRus && <FreeDeliveryBanner className={styles.freeDelivery} />}
       </div>
       <div className={styles.contrastRow}>
         <div className={styles.contentContainer}>
           <div className={styles.tabsHeader}>
-            <div className={styles.header}>Стоимость услуг</div>
+            <div className={styles.heading}>Стоимость услуг</div>
             {tabs && (
               <ButtonTabs
                 className={styles.tabs}
@@ -67,7 +87,7 @@ const PageDelivery: FC<PageDeliveryProps> = (props) => {
             })}
           >
             <div className={styles.table} dangerouslySetInnerHTML={{ __html: table.delivery }} />
-            {/* <div className={styles.table} dangerouslySetInnerHTML={{ __html: table.suburb }} />
+            {/*
             <div
               className={styles.table}
               dangerouslySetInnerHTML={{ __html: table.upliftFirstFloor }}
@@ -85,18 +105,80 @@ const PageDelivery: FC<PageDeliveryProps> = (props) => {
           <List
             className={styles.additionalCost}
             items={additionalCost}
-            renderChild={(paragraph: Document) => (
-              <div className={styles.paragraph}>{paragraph}</div>
+            renderChild={(paragraph: string) => (
+              <div className={styles.paragraph}>
+                <FormattedText currency={meta.currency}>{paragraph}</FormattedText>
+              </div>
             )}
           />
         </div>
       </div>
 
-      <div className={styles.contentContainer}>
-        <ImportantInfo
-          title='Важная информация:'
-          text='Мы ежедневно проверяем участников рейса на состояние здоровья перед выездом. У каждой команды доставки есть средства индивидуальной защиты: перчатки, маски и бахилы.'
-        />
+      <div className={cn(styles.contentContainer, { [styles.row]: true })}>
+        <SuburbTable className={styles.suburbTable} />
+
+        {attention && (
+          <ImportantInfo
+            className={styles.importantInfo}
+            title='Важная информация:'
+            text={attention}
+          />
+        )}
+
+        <div className={styles.upliftFirstFloor}>
+          Занос на 1-й этаж: 500 ₽
+          <br />
+          (для клиентов, проживающих на первом этаже и в частных домах)
+        </div>
+
+        {acceptanceRules && (
+          <div className={styles.rules}>
+            <div className={styles.heading}>
+              {isRus ? 'Правила приема мебели' : 'Получение товара'}
+            </div>
+            <div className={styles.rulesText}>
+              {isRus ? (
+                <>
+                  Соблюдение нескольких простых правил – гарантия того, что товар будет доставлен в
+                  сохранности.
+                </>
+              ) : (
+                <>
+                  Отнеситесь ответственно к получению товара, если хотите быть уверены в том, что он
+                  будет доставлен в сохранности. Не поручайте его детям или сторонним людям, чтобы
+                  избежать случайного повреждения при подъеме.
+                </>
+              )}
+            </div>
+
+            <div className={styles.wrapperList}>
+              {!isRus && <div className={styles.listTitle}>При получении товара:</div>}
+              <OrderedList
+                className={styles.list}
+                list={acceptanceRules}
+                currency={meta.currency}
+              />
+            </div>
+          </div>
+        )}
+
+        {acceptanceProduct && <Attention className={styles.attention} text={acceptanceProduct} />}
+      </div>
+      <div className={styles.bottomInfo}>
+        <div className={styles.bottomInfoContainer}>
+          <div className={styles.subtitle}>Доставка по всей России</div>
+          <div className={styles.textBlock}>
+            <div className={styles.text}>
+              Чтобы заказать товар в магазине сайте, свяжитесь с нашим менеджером по телефону 7
+              (495) 266-71-47.
+            </div>
+            <div className={styles.text}>Доставка по городу Москва и области – от 3 дней.</div>
+          </div>
+          <Link to='/' className={styles.moreLink} view='primary'>
+            Подробнее
+          </Link>
+        </div>
+        <Image className={styles.carImage} src={carImage} />
       </div>
     </div>
   );
