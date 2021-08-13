d410a65b1534165324d21962f1dc2e036cb54fe7
Yury Kundin <kundin.y@divan.ru>
2021-08-14 00:58:43 +0300

    feature: Custom route component with rules for regions

--- client/App/App.tsx
+++ client/App/App.tsx
@@ -1,9 +1,8 @@
 import React, { FC } from 'react';
 import loadable from '@loadable/component';
-import { Switch, Route } from 'react-router-dom';
 
 import ModalsProvider from '@Contexts/Modals/ModalsProvider';
-import ApiSetup from '@Components/ApiSetup';
+import Route from '@Components/Route';
 import '@UI/fonts.module.css';
 import '@UI/vars.module.css';
 import './App.css';
@@ -21,47 +20,21 @@ const RouteWarranty = loadable(() => import('@Routes/RouteWarranty'));
 const App: FC = () => {
   return (
     <ModalsProvider>
-      <Switch>
-        <Route exact path='/'>
-          <RouteIndex />
-        </Route>
+      <Route path='/'>
+        <RouteIndex />
+      </Route>
 
-        <Route exact path='/category/:slug'>
-          <ApiSetup>
-            <RouteCategory />
-          </ApiSetup>
-        </Route>
+      <Route path='/category/:slug'>
+        <RouteCategory />
+      </Route>
 
-        <Route exact path='/:region/category/:slug'>
-          <ApiSetup>
-            <RouteCategory />
-          </ApiSetup>
-        </Route>
+      <Route path='/product/:slug'>
+        <RouteProduct />
+      </Route>
 
-        <Route exact path='/product/:slug'>
-          <ApiSetup>
-            <RouteProduct />
-          </ApiSetup>
-        </Route>
-
-        <Route exact path='/:region/product/:slug'>
-          <ApiSetup>
-            <RouteProduct />
-          </ApiSetup>
-        </Route>
-
-        <Route exact path='/site/warranty'>
-          <ApiSetup>
-            <RouteWarranty />
-          </ApiSetup>
-        </Route>
-
-        <Route exact path='/:region/site/warranty'>
-          <ApiSetup>
-            <RouteWarranty />
-          </ApiSetup>
-        </Route>
-      </Switch>
+      <Route path='/site/warranty'>
+        <RouteWarranty />
+      </Route>
     </ModalsProvider>
   );
 };
---
+++ client/Components/Route/Route.tsx
@@ -0,0 +1,24 @@
+import React, { FC, memo } from 'react';
+import { Route as BaseRoute, RouteProps as BaseRouteProps } from 'react-router-dom';
+
+import { Api } from '@Api/index';
+import useRequest from '@Hooks/useRequest';
+import useMeta from '@Queries/useMeta';
+
+export interface RouteProps extends BaseRouteProps {
+  regional?: boolean;
+}
+
+const Route: FC<RouteProps> = (props) => {
+  const { path, exact = true, regional = true, ...restProps } = props;
+  const paths = [path, regional && `/:region${path}`].filter(Boolean) as string[];
+  const request = useRequest();
+  const meta = useMeta({ ssr: true });
+
+  Api.setRequest(request);
+  Api.setServices(meta.data?.services);
+
+  return <BaseRoute {...restProps} exact={exact} path={paths} />;
+};
+
+export default memo(Route);
