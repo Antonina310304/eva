e305b1f7e22a32e8a65afe2cc5237510434a94f9
Yury Kundin <kundin.y@divan.ru>
2021-08-15 22:40:11 +0300

    feature: Add icon for preview

--- client/Pages/PageProduct/elements/Characteristics/elements/MechanismPopup/MechanismPopup.tsx
+++ client/Pages/PageProduct/elements/Characteristics/elements/MechanismPopup/MechanismPopup.tsx
@@ -16,33 +16,30 @@ export interface MechanismPopupProps extends HTMLAttributes<HTMLDivElement> {
 
 const MechanismPopup = forwardRef<HTMLDivElement, MechanismPopupProps>((props, ref) => {
   const { visible, parameter, className, onClose, ...restProps } = props;
-  const [currentSrc, setCurrentSrc] = useState<string>(parameter.preview);
+  const [hovered, setHovered] = useState(false);
 
   const handleMouseEnterImage = useCallback(() => {
-    if (!parameter.image) return;
-
-    setCurrentSrc(parameter.image);
-  }, [parameter]);
+    setHovered(true);
+  }, []);
 
   const handleMouseLeaveImage = useCallback(() => {
-    if (!parameter.image) return;
-
-    setCurrentSrc(parameter.preview);
-  }, [parameter]);
+    setHovered(false);
+  }, []);
 
   return (
     <Popup {...restProps} ref={ref} className={cn(styles.popup, className)} visible={visible}>
       <div className={styles.container}>
         <IconClose className={styles.iconClose} view='default' size='s' onClick={onClose} />
 
-        <Image
-          src={currentSrc}
-          className={styles.img}
-          onMouseEnter={handleMouseEnterImage}
-          onMouseLeave={handleMouseLeaveImage}
-        >
-          {parameter.image && <div className={styles.iconPlay} />}
-        </Image>
+        <div className={styles.wrapperImage}>
+          <Image
+            src={hovered && parameter.image ? parameter.image : parameter.preview}
+            className={styles.img}
+            onMouseEnter={handleMouseEnterImage}
+            onMouseLeave={handleMouseLeaveImage}
+          />
+          {!hovered && parameter.image && <div className={styles.iconPlay} />}
+        </div>
 
         <div className={styles.content}>
           <div className={styles.title}>{parameter.title}</div>
