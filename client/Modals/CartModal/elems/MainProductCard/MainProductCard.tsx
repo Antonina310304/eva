4c6a8b67fe43bf7dd88f878e31809acfc9a47ff4
Yury Kundin <kundin.y@divan.ru>
2021-08-18 08:35:27 +0300

    feature: Move footer to component

--- client/Modals/CartModal/elems/MainProductCard/MainProductCard.tsx
+++ client/Modals/CartModal/elems/MainProductCard/MainProductCard.tsx
@@ -1,10 +1,10 @@
 import React, { FC, HTMLAttributes, memo } from 'react';
 import cn from 'classnames';
 
-import InputQuantity from '@UI/InputQuantity';
-import Price from '@UI/Price';
+import useMedias from '@Hooks/useMedias';
 import { CartProductData } from '@Types/Cart';
 import Preview from './elems/Preview';
+import Footer from './elems/Footer';
 import styles from './MainProductCard.module.css';
 
 export interface MainProductCardProps extends HTMLAttributes<HTMLDivElement> {
@@ -14,21 +14,28 @@ export interface MainProductCardProps extends HTMLAttributes<HTMLDivElement> {
 
 const MainProductCard: FC<MainProductCardProps> = (props) => {
   const { className, product, ...restProps } = props;
+  const { isMobile } = useMedias();
 
   return (
     <div {...restProps} className={cn(styles.card, className)}>
-      <Preview className={styles.preview} product={product} />
+      <div className={styles.main}>
+        <Preview className={styles.preview} product={product} />
 
-      <div className={styles.content}>
-        <div className={styles.name}>{product.name}</div>
-        <div className={styles.footer}>
-          <InputQuantity className={styles.quantity} />
-
-          <div className={styles.wrapperPrice}>
-            <Price className={styles.price} price={product.price} />
-          </div>
+        <div className={styles.content}>
+          <div className={styles.name}>{`${product.type} ${product.name}`}</div>
+          {!isMobile && (
+            <div className={styles.wrapperFooter}>
+              <Footer product={product} />
+            </div>
+          )}
         </div>
       </div>
+
+      {isMobile && (
+        <div className={styles.wrapperFooter}>
+          <Footer product={product} />
+        </div>
+      )}
     </div>
   );
 };
