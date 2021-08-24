ee30dcaba4bea7436fad8110dfe6c40d5372b7d9
Yury Kundin <kundin.y@divan.ru>
2021-08-24 12:41:39 +0300

    fix: Fix types

--- client/Stores/Cart/typings.ts
+++ client/Stores/Cart/typings.ts
@@ -1,5 +1,12 @@
+import { NetworkStatus } from '@Types/Base';
+import { CartData } from '@Types/Cart';
+
 export interface UseCartOpts {
   preload?: boolean;
 }
 
-export type UseCart = (opts?: UseCartOpts) => any;
+export interface UseCartResult extends CartData {
+  network: NetworkStatus;
+}
+
+export type UseCart = (opts?: UseCartOpts) => UseCartResult;
