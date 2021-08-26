530a523b82a6cd8073806d334eb566df5256c7ec
Yury Kundin <kundin.y@divan.ru>
2021-08-26 14:30:41 +0300

    feature: Add onChange event

--- client/UI/RadioGroup/RadioGroup.tsx
+++ client/UI/RadioGroup/RadioGroup.tsx
@@ -1,22 +1,40 @@
-import React, { FC, HTMLAttributes, memo } from 'react';
+import React, { FC, FormEvent, HTMLAttributes, memo, useCallback, useState } from 'react';
 import cn from 'classnames';
 
 import Radio, { RadioProps } from '@UI/Radio';
 import styles from './RadioGroup.module.css';
 
-export interface RadioGroupProps extends HTMLAttributes<HTMLDivElement> {
+export interface RadioGroupProps extends Omit<HTMLAttributes<HTMLDivElement>, 'onChange'> {
   className?: string;
   name?: string;
   items: RadioProps[];
+  onChange?: (e: FormEvent, item: RadioProps) => void;
 }
 
 const RadioGroup: FC<RadioGroupProps> = (props) => {
-  const { className, name, items, ...restProps } = props;
+  const { className, name, items, onChange, ...restProps } = props;
+  const [, setCheckedItem] = useState(() => {
+    return items.find((item) => item.defaultChecked || item.checked);
+  });
+
+  const handleChange = useCallback(
+    (e: FormEvent, item) => {
+      setCheckedItem(item);
+      if (onChange) onChange(e, item);
+    },
+    [onChange],
+  );
 
   return (
     <div {...restProps} className={cn(styles.group, className)}>
       {items.map((item, index) => (
-        <Radio className={styles.item} {...item} name={name} key={index} />
+        <Radio
+          className={styles.item}
+          {...item}
+          name={name}
+          key={index}
+          onChange={(e) => handleChange(e, item)}
+        />
       ))}
     </div>
   );
