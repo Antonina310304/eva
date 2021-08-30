db31c1f3d2444e9ad6df5119ee2f56b416ea417f
Torseeva Julia <torseeva.y@divan.ru>
2021-08-30 16:55:13 +0300

    feature: Add cart positions

--- client/Pages/PageDelivery/elements/CartBlock/CartBlock.tsx
+++ client/Pages/PageDelivery/elements/CartBlock/CartBlock.tsx
@@ -1,7 +1,9 @@
 import React, { FC, HTMLAttributes, memo } from 'react';
 import cn from 'classnames';
 
+import CrossSaleProductCard from '@Components/CrossSaleProductCard';
 import Price from '@UI/Price';
+import PositionsSection from './elements/PositionsSection';
 import styles from './CartBlock.module.css';
 
 export interface CartBlockProps extends HTMLAttributes<HTMLDivElement> {
@@ -18,6 +20,21 @@ const CartBlock: FC<CartBlockProps> = (props) => {
         <div className={styles.info}>
           {`В вашей корзине ${cart.count} товаров на сумму `}
           <Price price={cart.cost} />
+          {cart.positions?.length > 0 && (
+            <PositionsSection
+              className={styles.sectionCrossSale}
+              title='Ваша корзина'
+              products={cart.positions}
+              renderItem={(productCardProps) => (
+                <div className={styles.productItem}>
+                  <CrossSaleProductCard {...productCardProps} />
+                  <div className={styles.productCount}>
+                    {`Кол-во: ${productCardProps.quantity} шт.`}
+                  </div>
+                </div>
+              )}
+            />
+          )}
         </div>
       </div>
     </div>
---
+++ client/Pages/PageDelivery/elements/CartBlock/elements/PositionsSection/PositionsSection.tsx
@@ -0,0 +1,100 @@
+import React, {
+  FC,
+  HTMLAttributes,
+  ReactElement,
+  memo,
+  useCallback,
+  useState,
+  cloneElement,
+} from 'react';
+import cn from 'classnames';
+
+import Section from '@Components/Section';
+import Gallery, { ProgressOptions } from '@UI/Gallery';
+import NavArrows from '@UI/NavArrows';
+import ProgressBar from '@UI/ProgressBar';
+import { ProductData } from '@Types/Product';
+import styles from './PositionsSection.module.css';
+
+export interface RenderItem {
+  product: ProductData;
+}
+
+export interface PositionsSectionProps extends HTMLAttributes<HTMLDivElement> {
+  className?: string;
+  title: string;
+  products: ProductData[];
+  tabs?: ReactElement;
+  renderItem?: (props: RenderItem) => ReactElement;
+}
+
+const PositionsSection: FC<PositionsSectionProps> = (props) => {
+  const { className, title, products, tabs, renderItem, ...restProps } = props;
+  const [slide, setSlide] = useState(0);
+  const [track, setTrack] = useState<ProgressOptions>(null);
+
+  const normalizeSlide = useCallback(
+    (value: number) => {
+      if (value < 0) return 0;
+      if (value > products.length) return products.length;
+
+      return value;
+    },
+    [products.length],
+  );
+
+  const handleChangeCurrent = useCallback(({ current }) => {
+    setSlide(current);
+  }, []);
+
+  const handleChangeProgress = useCallback((opts: ProgressOptions) => {
+    setTrack(opts);
+  }, []);
+
+  const handlePrev = useCallback(() => {
+    setSlide((prev) => normalizeSlide(prev - 1));
+  }, [normalizeSlide]);
+
+  const handleNext = useCallback(() => {
+    if (track.finished) return;
+
+    setSlide((prev) => normalizeSlide(prev + 1));
+  }, [normalizeSlide, track]);
+
+  return (
+    <Section
+      {...restProps}
+      className={cn(styles.section, className)}
+      title={title}
+      additional={
+        <NavArrows
+          className={cn(styles.arrows, { [styles.visible]: track?.width < 100 })}
+          onPrev={handlePrev}
+          onNext={handleNext}
+        />
+      }
+    >
+      {tabs && <div className={styles.tabs}>{tabs}</div>}
+
+      <div className={styles.wrapperGallery}>
+        <Gallery
+          className={styles.gallery}
+          slideIndex={slide}
+          key={products.length}
+          onChangeCurrent={handleChangeCurrent}
+          onChangeProgress={handleChangeProgress}
+        >
+          {products.map((product) => {
+            const item = renderItem({ product });
+
+            return cloneElement(item, { ...item.props, key: product.id });
+          })}
+        </Gallery>
+
+        <ProgressBar track={track} />
+      </div>
+    </Section>
+  );
+};
+
+export default memo(PositionsSection);
