2d8dd19ebd4c625cf8db3f69f16f522f91ad8a31
Yury Kundin <kundin.y@divan.ru>
2021-09-01 15:19:34 +0300

    fix: Fix behavior when click on preview

--- client/Components/PressDetails/PressDetails.tsx
+++ client/Components/PressDetails/PressDetails.tsx
@@ -8,13 +8,8 @@ import Gallery, { ProgressOptions } from '@UI/Gallery';
 import useMedias from '@Hooks/useMedias';
 import Button from '@UI/Button';
 import Link from '@UI/Link';
-
+import { MetaDataSocial } from '@Types/Meta';
 import styles from './PressDetails.module.css';
-
-export interface SocialItem {
-  id: number;
-  link: string;
-}
 
 export interface ArticleImage {
   src: string;
@@ -31,93 +26,68 @@ export interface ArticleData {
 export interface PressDetailsProps {
   className?: string;
   article: ArticleData;
-  socials: SocialItem;
+  socials: MetaDataSocial[];
 }
 
 const PressDetails: FC<PressDetailsProps> = (props) => {
   const { className, article, socials, ...restProps } = props;
   const { link, logo, images, preview, text } = article;
   const { isMobileM } = useMedias();
-
-  const [slide, setSlide] = useState(0);
+  const [slideIndex, setSlideIndex] = useState<number>(0);
   const [track, setTrack] = useState<ProgressOptions>(null);
 
-  const normalizeSlide = useCallback(
-    (value: number) => {
-      if (value < 0) return 0;
-      if (value > images.length) return images.length;
+  const handleClickPreview = useCallback((_e, previewIndex) => {
+    if (window.cancelClick) return;
 
-      return value;
-    },
-    [images.length],
-  );
-
-  const [slideIndex, setSlideIndex] = useState(0);
-  const [slideIndexUp, setSlideIndexUp] = useState(0);
-
-  const handleCurrent = useCallback(() => {
-    setSlideIndex(slideIndexUp);
-  }, [slideIndexUp]);
+    setSlideIndex(previewIndex);
+  }, []);
 
   const handleChangeProgress = useCallback((opts: ProgressOptions) => {
     setTrack(opts);
   }, []);
 
-  const handlePrev = useCallback(() => {
-    setSlide((prev) => normalizeSlide(prev - 1));
-  }, [normalizeSlide]);
-
-  const handleNext = useCallback(() => {
-    if (track.finished) return;
-
-    setSlide((prev) => normalizeSlide(prev + 1));
-  }, [normalizeSlide, track]);
-
   return (
     <div {...restProps} className={cn(styles.pressDetails, [className])}>
       <div className={styles.carousel}>
         <div className={styles.wrapperMainGallery}>
-          <Gallery onChangeProgress={handleChangeProgress}>
+          <Gallery slideIndex={slideIndex}>
             {images.map((image, indexSlide) => (
-              <div className={styles.mainGalleryItem} slideIndexUp={slideIndexUp}>
-                <Image src={image.src} key={indexSlide} className={styles.mainGalleryPreview} />
+              <div className={styles.mainGalleryItem} key={indexSlide}>
+                <Image className={styles.mainGalleryPreview} src={image.src} />
               </div>
             ))}
           </Gallery>
         </div>
-        {isMobileM ? (
+
+        <div className={styles.wrapperSecondGallery}>
+          <Gallery
+            className={styles.secondGallery}
+            key={images.length}
+            onChangeProgress={handleChangeProgress}
+          >
+            {images.map((image, indexSlide) => (
+              <div
+                className={styles.secondGalleryItem}
+                key={indexSlide}
+                onClick={(e) => handleClickPreview(e, indexSlide)}
+              >
+                <Image className={styles.secondGalleryPreview} src={image.src} />
+              </div>
+            ))}
+          </Gallery>
+
           <ProgressBar className={styles.progressBar} track={track} />
-        ) : (
-          <div className={styles.wrapperSecondGallery}>
-            <Gallery
-              className={styles.secondGallery}
-              key={images.length}
-              onChangeProgress={handleChangeProgress}
-            >
-              {images.map((image, indexSlide) => (
-                <div
-                  className={styles.secondGalleryItem}
-                  key={indexSlide}
-                  slideIndex={slideIndex}
-                  onClick={handleCurrent}
-                >
-                  <Image className={styles.secondGalleryPreview} src={image.src} />
-                </div>
-              ))}
-            </Gallery>
-            <ProgressBar className={styles.progressBar} track={track} />
-          </div>
-        )}
+        </div>
       </div>
       <div className={styles.info}>
         <div className={styles.contentWrapper}>
           <div className={styles.heading}>{preview}</div>
           <div className={styles.text}>{text}</div>
-          <Button className={styles.button}>
-            <Link className={styles.link} to={link} target='_blank' view='default'>
+          <Link className={styles.link} to={link} target='_blank'>
+            <Button className={styles.button} theme='primary'>
               Читать статью
-            </Link>
-          </Button>
+            </Button>
+          </Link>
         </div>
         <div className={styles.contactsWrapper}>
           <div className={styles.socialsWrapper}>
