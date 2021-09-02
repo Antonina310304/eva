1a9de4c87fa517e235cab7d04d0799fed52f8338
Olga Ismailova <ismailova.o@divan.ru>
2021-09-02 19:59:10 +0500

    feature: Add gallery

--- client/Pages/PageB2bDetail/PageB2bDetail.tsx
+++ client/Pages/PageB2bDetail/PageB2bDetail.tsx
@@ -1,6 +1,11 @@
-import React, { FC, HTMLAttributes, memo } from 'react';
+import React, { FC, HTMLAttributes, memo, useState, useCallback } from 'react';
 import cn from 'classnames';
 
+import Section from '@Components/Section';
+import Gallery, { ProgressOptions } from '@UI/Gallery';
+import NavArrows from '@UI/NavArrows';
+import Image from '@UI/Image';
+import ProgressBar from '@UI/ProgressBar';
 import { PageB2bDetailData } from './typings';
 import styles from './PageB2bDetail.module.css';
 
@@ -12,7 +17,37 @@ export interface PageB2bDetailProps extends HTMLAttributes<HTMLDivElement> {
 
 const PageB2bDetail: FC<PageB2bDetailProps> = (props) => {
   const { className, page, ...restProps } = props;
-  const { title, teaser } = page;
+  const { title, teaser, projects, examples } = page;
+  const [slide, setSlide] = useState(0);
+  const [track, setTrack] = useState<ProgressOptions>(null);
+
+  const normalizeSlide = useCallback(
+    (value: number) => {
+      if (value < 0) return 0;
+      if (value > examples.length) return examples.length;
+
+      return value;
+    },
+    [examples.length],
+  );
+
+  const handleChangeCurrent = useCallback(({ current }) => {
+    setSlide(current);
+  }, []);
+
+  const handleChangeProgress = useCallback((opts: ProgressOptions) => {
+    setTrack(opts);
+  }, []);
+
+  const handlePrev = useCallback(() => {
+    setSlide((prev) => normalizeSlide(prev - 1));
+  }, [normalizeSlide]);
+
+  const handleNext = useCallback(() => {
+    if (track.finished) return;
+
+    setSlide((prev) => normalizeSlide(prev + 1));
+  }, [normalizeSlide, track]);
 
   return (
     <div {...restProps} className={cn(styles.page, className)}>
@@ -20,6 +55,30 @@ const PageB2bDetail: FC<PageB2bDetailProps> = (props) => {
       <div className={styles.teaserWrapper}>
         <div className={styles.teaser}>{teaser}</div>
       </div>
+      <h2 className={styles.subheading}>{projects.title}</h2>
+      <Section
+        {...restProps}
+        className={cn(styles.section, className)}
+        additional={<NavArrows className={styles.arrows} onPrev={handlePrev} onNext={handleNext} />}
+      >
+        <div className={styles.wrapperGallery}>
+          <Gallery
+            className={styles.gallery}
+            slideIndex={slide}
+            key={examples.length}
+            onChangeCurrent={handleChangeCurrent}
+            onChangeProgress={handleChangeProgress}
+          >
+            {(examples as any[]).map((item, index) => (
+              <div className={styles.galleryItem} key={index}>
+                <Image className={styles.galleryImage} src={item.src} />
+              </div>
+            ))}
+          </Gallery>
+
+          <ProgressBar className={styles.progressBar} track={track} />
+        </div>
+      </Section>
     </div>
   );
 };
--- client/Pages/PageB2bDetail/typings.tsx
+++ client/Pages/PageB2bDetail/typings.tsx
@@ -1,5 +1,19 @@
+export interface ExamplesData {
+  height: number;
+  projectId: number;
+  src: string;
+  text: string[];
+  title: string;
+  width: number;
+  length: number;
+}
+export interface ProjectItem {
+  title: string;
+}
 export interface PageB2bDetailData {
+  projects: ProjectItem;
   title: string;
   teaser: string;
+  examples: ExamplesData;
   test: any;
 }
