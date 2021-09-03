9b635543122022efb3d8ff7e9925ae3c7cbe9d45
Yury Kundin <kundin.y@divan.ru>
2021-09-03 17:34:52 +0300

    feature: Make sidebar sticky

--- client/Pages/PageOrderCheck/PageOrderCheck.tsx
+++ client/Pages/PageOrderCheck/PageOrderCheck.tsx
@@ -1,8 +1,9 @@
-import React, { FC, HTMLAttributes, memo } from 'react';
+import React, { FC, HTMLAttributes, memo, useRef, useEffect, useState, useCallback } from 'react';
 import cn from 'classnames';
 
 import OrderForm from '@Forms/OrderForm';
 import { useOrderForm } from '@Stores/OrderForm';
+import useMedias from '@Hooks/useMedias';
 import { Profile } from '@Types/Profile';
 import { PageOrderCheckData } from './typings';
 import WrapperForm from './elems/WrapperForm';
@@ -18,7 +19,56 @@ export interface PageOrderCheckProps extends HTMLAttributes<HTMLDivElement> {
 
 const PageOrderCheck: FC<PageOrderCheckProps> = (props) => {
   const { className, page, profile, ...restProps } = props;
+  const { isMobileM } = useMedias();
+  const [sidebarStyles, setSidebarStyles] = useState(null);
   const orderForm = useOrderForm();
+  const refContent = useRef<HTMLDivElement>(null);
+  const refSidebar = useRef<HTMLDivElement>(null);
+
+  const handleScroll = useCallback(() => {
+    if (!refContent.current || !refSidebar.current) return;
+
+    const rectContent = refContent.current.getBoundingClientRect();
+    const rectSidebar = refSidebar.current.getBoundingClientRect();
+
+    const boxContentBottom = Math.round(rectContent.bottom);
+    const boxContentTop = Math.round(rectContent.top);
+    const boxInfoBottom = Math.round(rectSidebar.bottom);
+    const boxInfoTop = Math.round(rectSidebar.top);
+
+    if (boxContentBottom <= boxInfoBottom && boxInfoTop <= 0) {
+      setSidebarStyles({
+        position: 'absolute',
+        bottom: 0,
+      });
+      return;
+    }
+
+    if (boxContentTop <= 0) {
+      setSidebarStyles({
+        position: 'fixed',
+        top: 0,
+        left: rectSidebar.left,
+      });
+    } else {
+      setSidebarStyles(null);
+    }
+  }, []);
+
+  // Плавающий блок информации в сайдбаре
+  useEffect(() => {
+    function cleanup() {
+      setSidebarStyles(null);
+      window.removeEventListener('scroll', handleScroll);
+    }
+
+    if (isMobileM) return cleanup;
+
+    handleScroll();
+    window.addEventListener('scroll', handleScroll);
+
+    return cleanup;
+  }, [handleScroll, isMobileM]);
 
   return (
     <div {...restProps} className={cn(styles.page, className)}>
@@ -27,7 +77,7 @@ const PageOrderCheck: FC<PageOrderCheckProps> = (props) => {
           <h1 className={styles.title}>Корзина</h1>
 
           <div className={styles.container}>
-            <div className={styles.content}>
+            <div className={styles.content} ref={refContent}>
               <ListOfPositions />
 
               <WrapperForm
@@ -47,6 +97,8 @@ const PageOrderCheck: FC<PageOrderCheckProps> = (props) => {
                 }}
                 prepaymentPercent={page.deliveryCostDescription}
                 profile={profile}
+                ref={refSidebar}
+                style={sidebarStyles}
               />
             </div>
           </div>
