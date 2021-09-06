d1b446c47dbad9c1d9804af1140015b99ed54a1b
Yury Kundin <kundin.y@divan.ru>
2021-09-06 16:21:35 +0300

    feature: Add method for getting info about delivery

--- client/Api/Order/index.ts
+++ client/Api/Order/index.ts
@@ -1,9 +1,13 @@
 import getBonuses from './getBonuses';
+import getDeliveryInfo from './getDeliveryInfo';
 import getInstallmentVariants from './getInstallmentVariants';
 import getOfertaInfo from './getOfertaInfo';
 
+export * from './getDeliveryInfo';
+
 export const ApiOrder = {
   getBonuses,
+  getDeliveryInfo,
   getInstallmentVariants,
   getOfertaInfo,
 };
