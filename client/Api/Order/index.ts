b28dea8ae3fcf71caf8bd68903e39ed469c58dbb
Yury Kundin <kundin.y@divan.ru>
2021-09-06 13:35:55 +0300

    feature: Add method getting info about offer agreement

--- client/Api/Order/index.ts
+++ client/Api/Order/index.ts
@@ -1,8 +1,10 @@
 import getBonuses from './getBonuses';
 import getInstallmentVariants from './getInstallmentVariants';
+import getOfertaInfo from './getOfertaInfo';
 
 export const ApiOrder = {
   getBonuses,
   getInstallmentVariants,
+  getOfertaInfo,
 };
 export default null;
