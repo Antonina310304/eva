ca11e392dc7a1ee7f1d2772aae637e6167b7a5e0
a.sekret@wssupport.ru <a.sekret@wssupport.ru>
2021-09-07 11:46:20 +0300

    fix mobile menu

--- client/Components/Header/elems/Subcategory/Subcategory.tsx
+++ client/Components/Header/elems/Subcategory/Subcategory.tsx
@@ -3,6 +3,7 @@ import { IMainNav } from '@Types/MainNav';
 import CategoryList from '@Components/Header/elems/CategoryList';
 import Link from '@UI/Link';
 import MobileNavContainer from '@Components/Header/elems/MobileNavContainer';
+import Scroller from '@UI/Scroller';
 import styles from './Subcategory.module.css';
 
 export interface SubcategoryProps extends HTMLAttributes<HTMLDivElement> {
@@ -12,7 +13,7 @@ export interface SubcategoryProps extends HTMLAttributes<HTMLDivElement> {
 
 const Subcategory: FC<SubcategoryProps> = ({ category }) => {
   return (
-    <div>
+    <div className={styles.subcategory}>
       <div className={styles.header}>
         <img src={category.img} alt={category.title} />
         <MobileNavContainer className={styles.titleBlock}>
@@ -25,21 +26,23 @@ const Subcategory: FC<SubcategoryProps> = ({ category }) => {
         </MobileNavContainer>
       </div>
       <MobileNavContainer className={styles.inner}>
-        <>
-          {category.dropDown.map((categoryItem) => {
-            return (
-              <div key={categoryItem.name} className={styles.section}>
-                <div className={styles.wrapper}>
-                  <p className={styles.type}>{categoryItem.name}</p>
-                  <Link view='grayString' to={categoryItem.link}>
-                    {categoryItem.textLink}
-                  </Link>
+        <Scroller className={styles.scroller}>
+          <div className={styles.scrollerInner}>
+            {category.dropDown.map((categoryItem) => {
+              return (
+                <div key={categoryItem.name} className={styles.section}>
+                  <div className={styles.wrapper}>
+                    <p className={styles.type}>{categoryItem.name}</p>
+                    <Link view='grayString' to={categoryItem.link}>
+                      {categoryItem.textLink}
+                    </Link>
+                  </div>
+                  <CategoryList category={categoryItem} />
                 </div>
-                <CategoryList category={categoryItem} />
-              </div>
-            );
-          })}
-        </>
+              );
+            })}
+          </div>
+        </Scroller>
       </MobileNavContainer>
     </div>
   );
