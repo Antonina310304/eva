05cb8ccce6993bf28730a7a5d1c6ce96937d0286
Yury Kundin <kundin.y@divan.ru>
2021-09-09 13:02:59 +0300

    fix: Fix path for logotype

--- client/Components/Footer/elems/FooterLogo/FooterLogo.tsx
+++ client/Components/Footer/elems/FooterLogo/FooterLogo.tsx
@@ -1,7 +1,6 @@
 import React, { FC, HTMLAttributes, memo } from 'react';
 
 import Link from '@UI/Link';
-import logoSrc from './FooterLogo.svg';
 import styles from './FooterLogo.module.css';
 
 export interface FooterProps extends HTMLAttributes<HTMLDivElement> {
@@ -11,7 +10,7 @@ export interface FooterProps extends HTMLAttributes<HTMLDivElement> {
 const FooterLogo: FC<FooterProps> = () => {
   return (
     <Link className={styles.footerLogo} to='/'>
-      <img src={logoSrc} alt='Логотип divan.ru' />
+      <img src='/react/static/img/FooterLogo.svg' alt='Логотип divan.ru' />
     </Link>
   );
 };
