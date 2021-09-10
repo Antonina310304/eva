c0c8d68c73abaef4b0ed5837f5c922efddd43c57
Yury Kundin <kundin.y@divan.ru>
2021-09-10 11:21:09 +0300

    fix: Add getting meta info

--- client/Routes/RouteB2bDetail/RouteB2bDetail.tsx
+++ client/Routes/RouteB2bDetail/RouteB2bDetail.tsx
@@ -4,15 +4,17 @@ import { useLocation } from 'react-router-dom';
 import usePage from '@Queries/usePage';
 import TemplateMain from '@Templates/TemplateMain';
 import PageB2bDetail from '@Pages/PageB2bDetail';
+import useMeta from '@Queries/useMeta';
 
 const RouteWarranty: FC = () => {
   const { pathname } = useLocation();
   const page = usePage({ path: pathname, ssr: true });
+  const meta = useMeta({ ssr: true });
 
-  if (!page.isSuccess) return null;
+  if (!page.isSuccess || !meta.isSuccess) return null;
 
   return (
-    <TemplateMain>
+    <TemplateMain meta={meta.data}>
       <PageB2bDetail page={page.data as any} />
     </TemplateMain>
   );
