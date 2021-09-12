ad50c7fa4b9efa21b6073423c1bff675eadd9c89
Yury Kundin <kundin.y@divan.ru>
2021-09-12 22:00:23 +0300

    test: Fix types

--- cypress/integration/pages/category/regions.spec.ts
+++ cypress/integration/pages/category/regions.spec.ts
@@ -1,6 +1,6 @@
-const regions = ['ufa', 'vladimir'];
-
 describe('The page of category  products', () => {
+  const regions = ['ufa', 'vladimir'];
+
   regions.forEach((region) => {
     it(`successfully loads category: divany-i-kresla, region: ${region}`, () => {
       cy.visit(`/${region}/category/divany-i-kresla`);
--- cypress/integration/pages/product/regions.spec.ts
+++ cypress/integration/pages/product/regions.spec.ts
@@ -1,6 +1,6 @@
-const regions = ['tula', 'kazan'];
-
 describe('The page of product', () => {
+  const regions = ['tula', 'kazan'];
+
   regions.forEach((region) => {
     it(`successfully loads product: divan-dins-velvet-yellow, region: ${region}`, () => {
       cy.visit(`/${region}/product/divan-dins-velvet-yellow`);
