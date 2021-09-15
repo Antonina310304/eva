214b80229a3766d88457b8feee559637dd5ca19d
asuroedov <a.suredov@yandex.ru>
2021-09-15 10:15:18 +0300

    исправления отображения карточек в секции "актуальные скидки"

--- client/Pages/PagePromotionsDiscounts/elems/CurrentDiscounts/CurrentDiscountCard/CurrentDiscountsCard.tsx
+++ client/Pages/PagePromotionsDiscounts/elems/CurrentDiscounts/CurrentDiscountCard/CurrentDiscountsCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CategoryInterface } from '@Pages/PagePromotionsDiscounts/elems/CurrentDiscounts/Mock/mockCategories';
 import cn from 'classnames';
+import Link from '@UI/Link';
 import styles from './CurrentDiscountsCard.module.css';
 
 interface CurrentDiscountsCardProps {
@@ -14,15 +15,17 @@ const CurrentDiscountsCard: React.FC<CurrentDiscountsCardProps> = ({ className, 
       className={cn(styles.wrapper, className)}
       style={{ background: `url(${category.imageUrl})`, backgroundSize: 'cover' }}
     >
-      <div className={styles.content}>
-        <div className={styles.header}>
-          <div className={styles.name}>{category.categoryName}</div>
-          <div className={styles.count}>{category.countOfPositions}</div>
+      <Link to={category.link} className={styles.link}>
+        <div className={styles.content}>
+          <div className={styles.header}>
+            <div className={styles.name}>{category.categoryName}</div>
+            <div className={styles.count}>{category.countOfPositions}</div>
+          </div>
+          <div className={styles.footer}>
+            <div className={styles.discount}>{`до ${category.maxDiscount}%`}</div>
+          </div>
         </div>
-        <div className={styles.footer}>
-          <div className={styles.discount}>{`до ${category.maxDiscount}%`}</div>
-        </div>
-      </div>
+      </Link>
     </div>
   );
 };
--- client/Pages/PagePromotionsDiscounts/elems/CurrentDiscounts/Mock/mockCategories.ts
+++ client/Pages/PagePromotionsDiscounts/elems/CurrentDiscounts/Mock/mockCategories.ts
@@ -3,6 +3,7 @@ export interface CategoryInterface {
   countOfPositions: number;
   maxDiscount: number;
   imageUrl: string;
+  link: string;
 }
 
 export const mockCategoriesForCurrentDiscounts: CategoryInterface[] = [
@@ -11,48 +12,56 @@ export const mockCategoriesForCurrentDiscounts: CategoryInterface[] = [
     countOfPositions: 214,
     imageUrl: 'react/static/mockImg/PromotionsDiscount/CurrentDiscounts/1.png',
     maxDiscount: -50,
+    link: '/akcii-skidki',
   },
   {
     categoryName: 'Кровати',
     countOfPositions: 214,
     imageUrl: 'react/static/mockImg/PromotionsDiscount/CurrentDiscounts/2.png',
     maxDiscount: -50,
+    link: '/akcii-skidki',
   },
   {
     categoryName: 'Столы',
     countOfPositions: 214,
     imageUrl: 'react/static/mockImg/PromotionsDiscount/CurrentDiscounts/3.png',
     maxDiscount: -50,
+    link: '/akcii-skidki',
   },
   {
     categoryName: 'Стулья',
     countOfPositions: 214,
     imageUrl: 'react/static/mockImg/PromotionsDiscount/CurrentDiscounts/4.png',
     maxDiscount: -50,
+    link: '/akcii-skidki',
   },
   {
     categoryName: 'Кресла',
     countOfPositions: 214,
     imageUrl: 'react/static/mockImg/PromotionsDiscount/CurrentDiscounts/5.png',
     maxDiscount: -50,
+    link: '/akcii-skidki',
   },
   {
     categoryName: 'Тумбы',
     countOfPositions: 214,
     imageUrl: 'react/static/mockImg/PromotionsDiscount/CurrentDiscounts/6.png',
     maxDiscount: -50,
+    link: '/akcii-skidki',
   },
   {
     categoryName: 'Матрасы',
     countOfPositions: 214,
     imageUrl: 'react/static/mockImg/PromotionsDiscount/CurrentDiscounts/7.png',
     maxDiscount: -50,
+    link: '/akcii-skidki',
   },
   {
     categoryName: 'Шкафы',
     countOfPositions: 214,
     imageUrl: 'react/static/mockImg/PromotionsDiscount/CurrentDiscounts/8.png',
     maxDiscount: -50,
+    link: '/akcii-skidki',
   },
 
   {
@@ -60,47 +69,55 @@ export const mockCategoriesForCurrentDiscounts: CategoryInterface[] = [
     countOfPositions: 214,
     imageUrl: 'react/static/mockImg/PromotionsDiscount/CurrentDiscounts/1.png',
     maxDiscount: -50,
+    link: '/akcii-skidki',
   },
   {
     categoryName: 'Кровати 2',
     countOfPositions: 214,
     imageUrl: 'react/static/mockImg/PromotionsDiscount/CurrentDiscounts/2.png',
     maxDiscount: -50,
+    link: '/akcii-skidki',
   },
   {
     categoryName: 'Столы 2',
     countOfPositions: 214,
     imageUrl: 'react/static/mockImg/PromotionsDiscount/CurrentDiscounts/3.png',
     maxDiscount: -50,
+    link: '/akcii-skidki',
   },
   {
     categoryName: 'Стулья 2',
     countOfPositions: 214,
     imageUrl: 'react/static/mockImg/PromotionsDiscount/CurrentDiscounts/4.png',
     maxDiscount: -50,
+    link: '/akcii-skidki',
   },
   {
     categoryName: 'Кресла 2',
     countOfPositions: 214,
     imageUrl: 'react/static/mockImg/PromotionsDiscount/CurrentDiscounts/5.png',
     maxDiscount: -50,
+    link: '/akcii-skidki',
   },
   {
     categoryName: 'Тумбы 2',
     countOfPositions: 214,
     imageUrl: 'react/static/mockImg/PromotionsDiscount/CurrentDiscounts/6.png',
     maxDiscount: -50,
+    link: '/akcii-skidki',
   },
   {
     categoryName: 'Матрасы 2',
     countOfPositions: 214,
     imageUrl: 'react/static/mockImg/PromotionsDiscount/CurrentDiscounts/7.png',
     maxDiscount: -50,
+    link: '/akcii-skidki',
   },
   {
     categoryName: 'Шкафы 2',
     countOfPositions: 214,
     imageUrl: 'react/static/mockImg/PromotionsDiscount/CurrentDiscounts/8.png',
     maxDiscount: -50,
+    link: '/akcii-skidki',
   },
 ];
