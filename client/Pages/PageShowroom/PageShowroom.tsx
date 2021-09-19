96bb1e9c5c97c545ccd3180b3e5466f631397a2d
Yury Kundin <kundin.y@divan.ru>
2021-09-19 12:47:10 +0300

    feature: Add map with showrooms

--- client/Pages/PageShowroom/PageShowroom.tsx
+++ client/Pages/PageShowroom/PageShowroom.tsx
@@ -1,6 +1,7 @@
 import React, { FC, HTMLAttributes, memo } from 'react';
 
 import ServicePageTitle from '@Components/ServicePageTitle';
+import SectionShowroomsMap from '@Components/SectionShowroomsMap';
 import MainBanner from './elems/MainBanner';
 import styles from './PageShowroom.module.css';
 
@@ -8,11 +9,21 @@ export interface PageShowRoomProps extends HTMLAttributes<HTMLDivElement> {
   page: any;
 }
 
-const PageShowRoom: FC<PageShowRoomProps> = () => {
+const PageShowRoom: FC<PageShowRoomProps> = (props) => {
+  const { page } = props;
+
   return (
     <div className={styles.page}>
       <ServicePageTitle className={styles.title} title='Адреса магазинов' />
       <MainBanner className={styles.banner} />
+
+      <div className={styles.container}>
+        <SectionShowroomsMap
+          className={styles.map}
+          datasForMap={page.map}
+          pickupPoints={page.sellPoints}
+        />
+      </div>
     </div>
   );
 };
