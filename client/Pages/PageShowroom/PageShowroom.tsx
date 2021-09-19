f434abc0e1b6d7a3309ac7aa676d522d9d7b3038
Yury Kundin <kundin.y@divan.ru>
2021-09-19 13:02:09 +0300

    feature: Add sellpoints

--- client/Pages/PageShowroom/PageShowroom.tsx
+++ client/Pages/PageShowroom/PageShowroom.tsx
@@ -3,6 +3,7 @@ import React, { FC, HTMLAttributes, memo } from 'react';
 import ServicePageTitle from '@Components/ServicePageTitle';
 import SectionShowroomsMap from '@Components/SectionShowroomsMap';
 import MainBanner from './elems/MainBanner';
+import SellPoints from './elems/SellPoints';
 import styles from './PageShowroom.module.css';
 
 export interface PageShowRoomProps extends HTMLAttributes<HTMLDivElement> {
@@ -16,6 +17,8 @@ const PageShowRoom: FC<PageShowRoomProps> = (props) => {
     <div className={styles.page}>
       <ServicePageTitle className={styles.title} title='Адреса магазинов' />
       <MainBanner className={styles.banner} />
+
+      <SellPoints sellPoints={page.sellPoints} />
 
       <div className={styles.container}>
         <SectionShowroomsMap
