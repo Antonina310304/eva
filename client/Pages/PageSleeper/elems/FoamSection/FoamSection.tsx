64f65db3d7b4218da210080bf6f99d48c12da3fd
asuroedov <a.suredov@yandex.ru>
2021-09-23 11:38:01 +0300

    исправления в FoamSection

--- client/Pages/PageSleeper/elems/FoamSection/FoamSection.tsx
+++ client/Pages/PageSleeper/elems/FoamSection/FoamSection.tsx
@@ -1,44 +1,29 @@
 import React, { FC, memo } from 'react';
+
+import SleeperFeatures from '@Pages/PageSleeper/elems/FoamSection/SleeperFeatures/SleeperFeatures';
 import styles from './FoamSection.module.css';
 
-const text = [
+const sleeperFeatures = [
   {
-    title: 'Пена с эффектом памяти',
+    name: 'Пена с эффектом памяти',
     description:
       'Anatomic Foam нежно повторяет контуры тела и поддерживает позвоночник в правильном положении. Особая пористая текстура мягко обволакивает и окружает комфортом со всех сторон, снимает напряжение мышц спины и шеи, дарит ощущение сна в невесомости.',
+    image: 'react/static/img/sleeper/foamSection/bottomBlock1.png',
   },
   {
-    title: 'Удобный съемный чехол',
+    name: 'Удобный съемный чехол',
     description:
       'Нежная, приятная на ощупь поверхность матраса Sleeper — качественный трикотаж American Lady Border, гарантирует прекрасный воздухообмен. Боковины выполнены из рогожки Kiton. Благодаря застежке-молнии, предусмотренной по периметру чехла, его легко снять. Рекомендуем ручную или машинную стирку в деликатном режиме.',
+    image: 'react/static/img/sleeper/foamSection/bottomBlock2.png',
   },
 ];
-
-const firstImg = 'react/static/img/sleeper/foamSection/bottomBlock1.png';
-const secondImg = 'react/static/img/sleeper/foamSection/bottomBlock2.png';
 
 const FoamSection: FC = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.topBlock}>Три слоя инновационной пены для безупречного комфорта</div>
 
-      <div className={styles.bottomBlock}>
-        <div className={styles.bottomBlock_firstLine}>
-          <div className={styles.bottomBlock_textWrapper}>
-            <div className={styles.bottomBlock_title}>{text[0].title}</div>
-            <div className={styles.bottomBlock_description}>{text[0].description}</div>
-          </div>
-          <img className={styles.bottomBlock_img} src={firstImg} />
-        </div>
-
-        <div className={styles.bottomBlock_secondLine}>
-          <img className={styles.bottomBlock_img} src={secondImg} />
-          <div className={styles.bottomBlock_textWrapper}>
-            <div className={styles.bottomBlock_title}>{text[1].title}</div>
-            <div className={styles.bottomBlock_description}>{text[1].description}</div>
-          </div>
-        </div>
-      </div>
+      <SleeperFeatures features={sleeperFeatures} />
     </div>
   );
 };
